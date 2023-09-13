ac33f2c2d0bd40d02f526365b814fbf8ce878523
phamtuan19 <phamtuan19hd@gmail.com>
2023-09-13 23:27:17 +0700

    update

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use(
         * đã được cấu hình để sử dụng withCredentials (nếu bạn sử dụng Axios hoặc XMLHttpRequest) 
         * hoặc credentials: 'include' (nếu bạn sử dụng Fetch API) để xử lý các thông tin xác thực này.
         */
-        origin: "*",
+        // origin: "*",
         credentials: true,
         methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
         preflightContinue: true
